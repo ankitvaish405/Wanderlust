90ec7c8677f639430a166e36a66aacca2eed25b3
ankit vaish <ankitvaish405@gmail.com>
2025-01-22 02:27:18 +0530

    Initial commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dburl = process.env.ATLASDB_URL;
+const MONGO_URL = ("mongodb://127.0.0.1:27017/wanderlust");
 
 main()
     .then(() => {
@@ -30,7 +30,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(dburl);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
@@ -41,7 +41,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl: dburl,
+    mongoUrl: MONGO_URL,
     crypto: {
         secret: process.env.SECRET,
     },
