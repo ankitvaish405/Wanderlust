15f9a0e866a1a5face46905f39d78c1009b4eb74
ankit vaish <ankitvaish405@gmail.com>
2025-01-21 19:29:40 +0530

    Initial commit

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dburl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
+const dburl = process.env.ATLASDB_URL;
 
 main()
+    .connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("connected to DB");
     })
