d5e840299bb065e8c4f6359d46345503e420cea2
ankit vaish <ankitvaish405@gmail.com>
2025-01-21 19:14:39 +0530

    Initial commit

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = ("mongodb://127.0.0.1:27017/wanderlust");
-const dburl = process.env.ATLASDB_URL;
+const dburl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then(() => {
